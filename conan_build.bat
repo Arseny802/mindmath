ea8a2984adb21297e2dc31dbc0022ce1e0cdf067
Arseny <Arseny802@yandex.ru>
2023-09-22 22:22:17 +0300

    installer for x86

--- conan_build.bat
+++ conan_build.bat
@@ -10,17 +10,29 @@ set GENERATOR_TOOLSET=v143
 set CPP_STANDARD=17
 
 rem Create conan debug info
-conan install . --output-folder=cmake-build-debug --build=missing --settings=build_type=Debug -s compiler.cppstd=%CPP_STANDARD%
+conan install . --output-folder=cmake-build-debug --build=missing^
+ -s build_type=Debug -s compiler.cppstd=%CPP_STANDARD%
 cd cmake-build-debug
 cmake .. -G %GENERATOR% -DCMAKE_TOOLCHAIN_FILE=%TOOLCHAIN_FILE% -CMAKE_GENERATOR_PLATFORM=%GENERATOR_PLATFORM%^
  -CMAKE_GENERATOR_TOOLSET=%GENERATOR_TOOLSET% -DCMAKE_BUILD_TYPE=Debug
 cmake --build . --config Debug
 cd ..
 
-rem Create conan release info
-conan install . --output-folder=cmake-build-release --build=missing --settings=build_type=Release -s compiler.cppstd=%CPP_STANDARD%
-cd cmake-build-release
+rem Create conan release-64 info
+conan install . --output-folder=cmake-build-release-%GENERATOR_PLATFORM% --build=missing^
+ -s build_type=Release -s compiler.cppstd=%CPP_STANDARD%
+cd cmake-build-release-%GENERATOR_PLATFORM%
 cmake .. -G %GENERATOR% -DCMAKE_TOOLCHAIN_FILE=%TOOLCHAIN_FILE% -CMAKE_GENERATOR_PLATFORM=%GENERATOR_PLATFORM%^
- -CMAKE_GENERATOR_TOOLSET=%GENERATOR_TOOLSET% -DCMAKE_BUILD_TYPE=Release
+ -CMAKE_GENERATOR_TOOLSET=%GENERATOR_TOOLSET% -DCMAKE_BUILD_TYPE=Release -A x64
+cmake --build . --config Release
+cd ..
+
+set GENERATOR_PLATFORM=x86
+rem Create conan release-86 info
+conan install . --output-folder=cmake-build-release-%GENERATOR_PLATFORM% --build=missing^
+ -s build_type=Release -s compiler.cppstd=%CPP_STANDARD% -s arch=x86
+cd cmake-build-release-%GENERATOR_PLATFORM%
+cmake .. -G %GENERATOR% -DCMAKE_TOOLCHAIN_FILE=%TOOLCHAIN_FILE% -CMAKE_GENERATOR_PLATFORM=%GENERATOR_PLATFORM%^
+ -CMAKE_GENERATOR_TOOLSET=%GENERATOR_TOOLSET% -DCMAKE_BUILD_TYPE=Release -A Win32
 cmake --build . --config Release
 cd ..
--- cpack_run.bat
+++ cpack_run.bat
@@ -1,5 +1,15 @@
 @echo off
 
 cd bin
+
+echo Packing for Win32
+cd Win32
 cpack --config CPackConfig.cmake
 cpack --config CPackSourceConfig.cmake
+cd ..
+
+echo Packing for x64
+cd x64
+cpack --config CPackConfig.cmake
+cpack --config CPackSourceConfig.cmake
+cd ..
